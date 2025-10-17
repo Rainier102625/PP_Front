58d9fc02a082291513e062c5759971e16188359a
sssiion <rta2752775@gmail.com>
2025-10-17 13:45:05 +0900

    db
    데이터베이스 주소 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
             const time = format(finalDateTime, "HH:mm:ss");
             const categoryQuery = selectedCategory || "";
 
-            const apiUrl = `https://pp-production-d014.up.railway.app/api/recommend/?lat=${lat}&lon=${lon}&time=${time}&type=${categoryQuery}&radius=8000`;
+            const apiUrl = `https://pp-domain:8081/api/recommend/?lat=${lat}&lon=${lon}&time=${time}&type=${categoryQuery}&radius=8000`;
 
             console.log("Requesting API URL:", apiUrl);
 
